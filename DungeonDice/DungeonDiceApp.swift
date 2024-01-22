dc5819e45934c6bd9bd33972f2a88bf3dbd616d3
Gerd Faedtke <gerd.faedtke@icloud.com>
2024-01-22 14:39:13 +0100

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Gerd Faedtke on 22.01.24.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
